c982a85a31c8d76d0a17847ea1c83e42ac2ff20b
Tyler Hall <thall2980@gmail.com>
2022-10-13 20:03:07 -0400

    commit tree 7ae12c82e3e9d31b2b7a4e9084d57404c75f21d6

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 35, "M", "Brown", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 35, "M", "Pink", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Brown", "Grumpy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Brown", "Chill", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 125, "F", "Green", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 345, "M", "Purple", "Grumpy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "White", "Sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "Black", "Grumpy", 1);
